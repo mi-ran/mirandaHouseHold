8940f887833ad31afb1424e3e0df0762c0960675
mi-ran <azsxdc8911@gmail.com>
2020-12-03 23:12:50 +0900

    [mi-ran/mirandaHouseHold#5] 메인화면과 로그인 화면의 뷰 컨트롤러 추가

--- src/main/java/com/example/demo/web/mvc/controller/view/MainController.java
+++ src/main/java/com/example/demo/web/mvc/controller/view/MainController.java
@@ -1,15 +1,19 @@
-package com.example.demo.web.mvc.controller;
+package com.example.demo.web.mvc.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	/**
+	 * 메인 화면을 리턴한다.
+	 * @return
+	 */
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String index() {
-		System.out.println("jmrjmr MAIN");
-		return "NewFile.html";
+		return "index.html";
 	}
 }
