c0facf1338d452de49e4470ffba5b726883d059b
mi-ran <azsxdc8911@gmail.com>
2020-12-24 00:04:13 +0900

    [mi-ran/mirandaHousehold#14] 로그아웃 기능 추가

--- src/main/java/com/example/demo/web/mvc/controller/view/LoginController.java
+++ src/main/java/com/example/demo/web/mvc/controller/view/LoginController.java
@@ -1,8 +1,11 @@
 package com.example.demo.web.mvc.controller.view;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -14,5 +17,17 @@ public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login.html";
+	}
+	
+	/**
+	 * 로그아웃 시킨다.
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public Boolean logout(HttpSession session) {
+		session.removeAttribute("LOGIN_ID");
+		System.out.println("logout");
+		return true;
 	}
 }
