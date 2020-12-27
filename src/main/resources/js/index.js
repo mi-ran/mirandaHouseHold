7ecd2cb4dcc5f902bd284f2b1efa9c212e2a451b
mi-ran <azsxdc8911@gmail.com>
2020-12-27 20:37:47 +0900

    [mi-ran/mirandaHousehold#18] index페이지에서 assetView 페이지로 연결

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,8 +1,14 @@
-var index = {
+var assetIndex = 0;
+var debtIndex = 0;
+
+var index = {		
 		init : function() {
 			var _this = this;
 			$('.logoutbt').on('click', function () {
-				_this.doLogin();
+				_this.logout();
+			});
+			$('.jasanarea').on('click', function() {
+				_this.goAssetView();
 			});
             $('.plus1').on('click',function(){
                $('.madal_layer').show();
@@ -13,10 +19,24 @@ var index = {
                $('.modal').hide();
             });
 		},
-		doLogin : function() {
+		logout : function() {
 			$.ajax({
 				type: 'GET',
 				url: '/logout',
+				dataType: 'json',
+				contentType: 'application/json; charset=utf-8',
+			}).done(function(result) {
+				window.location.href="login";
+			}).fail(function (error) {
+				alert(error);
+			});
+		},
+		goAssetView : function() {
+			var assetList = $('#assetList');
+			alert(assetList[0]);
+			$.ajax({
+				type: 'GET',
+				url: '/assetView?assetId=',
 				dataType: 'json',
 				contentType: 'application/json; charset=utf-8',
 			}).done(function(result) {
