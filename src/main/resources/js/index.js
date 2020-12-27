06cccbf663c6501ea9e8eb8d0f9127bbfb2c73b5
조찬미 <cksal5438@gmail.com>
2020-12-27 20:18:34 +0900

    메인 및 로그인 페이지 모달페이지 등록 및 수정

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -4,6 +4,14 @@ var index = {
 			$('.logoutbt').on('click', function () {
 				_this.doLogin();
 			});
+            $('.plus1').on('click',function(){
+               $('.madal_layer').show();
+               $('.modal').show();
+            });
+            $('.closebt').on('click',function(){
+               $('.madal_layer').hide();
+               $('.modal').hide();
+            });
 		},
 		doLogin : function() {
 			$.ajax({
@@ -20,3 +28,15 @@ var index = {
 };
 
 index.init();
+    
+            $(document).ready(function(){     
+                for (var i = 2021; i<=3500; i++) {
+                    $('.years').append(new Option(i));
+                }
+                for (var i = 1; i<=12; i++) {
+                    $('.months').append(new Option(i));
+                }
+                for (var i =1; i<=31; i++) {
+                    $('.days').append(new Option(i));
+                }
+            });
