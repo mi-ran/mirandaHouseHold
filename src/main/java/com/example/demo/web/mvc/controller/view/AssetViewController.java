a9814c2a356bdc02f63cedfc41cee8152fe4c417
mi-ran <azsxdc8911@gmail.com>
2021-01-03 01:05:56 +0900

    [mi-ran/mirandaHousehold#21] 내역 초기 화면에 categoryList를 전달하도록 수정

--- src/main/java/com/example/demo/web/mvc/controller/view/AssetViewController.java
+++ src/main/java/com/example/demo/web/mvc/controller/view/AssetViewController.java
@@ -13,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.web.mvc.model.Asset;
+import com.example.demo.web.mvc.model.Category;
 import com.example.demo.web.repository.AssetRepository;
+import com.example.demo.web.repository.CategoryRepository;
 
 @Controller
 public class AssetViewController {
 
 	@Autowired
 	AssetRepository assetRepository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 	
 	/**
 	 * 자산 뷰를 리턴한다.
@@ -28,9 +33,11 @@ public class AssetViewController {
 	public String assetView(HttpSession session, Model model, @RequestParam(defaultValue = "") String assetId) {
 		String userId = (String)session.getAttribute("LOGIN_ID");
 		Optional<Asset> asset = assetRepository.findById(assetId);
+		List<Category> cateList = categoryRepository.findByUserId(userId);
 		
 		model.addAttribute("name", userId);
 		model.addAttribute("asset", asset.get());
+		model.addAttribute("categoryList", cateList);
 		return "naeyuck.html";
 	}
 }
