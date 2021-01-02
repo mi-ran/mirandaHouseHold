db4c4e4e65233bd28398bd6192ed5aa16e0812eb
mi-ran <azsxdc8911@gmail.com>
2021-01-03 00:15:27 +0900

    [mi-ran/mirandaHousehold#21] 내역 추가 기능

--- src/main/resources/js/asset.js
+++ src/main/resources/js/asset.js
@@ -4,6 +4,25 @@ var asset = {
 			$('.logoutbt').on('click', function () {
 				_this.logout();
 			});
+			
+			$('.pbt').on('click',function(){
+	           $('.madal_layer').removeClass('hide');
+	           $('.modal').removeClass('hide');
+	        });
+	            
+	        $('.closebt').on('click',function(){
+	           $('.madal_layer').addClass('hide');
+	           $('.modal').addClass('hide');
+	        });
+
+	        $(".js-example-tokenizer").select2({
+	            tags: true,
+	            tokenSeparators: [',', ' ']
+	        });
+	        
+	        $(".addbt").on('click', function() {
+	        	_this.addRecord();
+	        });
 		},
 		logout : function() {
 			$.ajax({
@@ -17,6 +36,49 @@ var asset = {
 				alert(error);
 			});
 		},
+		addRecord : function() {
+			var spendOrimport = $('input[name=money]:checked').val();
+			var money = $('.money').val();
+			var data = {
+				"assetId" : assetObj.id,
+				"userId" : userId,
+				"date" : $("select[name=years]").val() + '-' + $("select[name=months]").val() + '-' + $("select[name=days]").val(),
+				"about" : $('.meony_n').val(),
+				"spend" : spendOrimport == "out" ? money : 0,
+				"import_" : spendOrimport == "get" ? money : 0,
+				"account" : 0,
+				"categoryId" : $("select[name=category]").val()
+			}
+			
+			$.ajax({
+				type: 'PUT',
+				url: '/record/' + userId + '/',
+				dataType: 'json',
+				contentType: 'application/json; charset=utf-8',
+				data : JSON.stringify(data),
+			}).done(function(result) {
+				alert("SUCESS");
+			}).fail(function (error) {
+				alert(error);
+			});
+		}
 };
 
 asset.init();
+
+function pad(n, width) {
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+
+$(document).ready(function(){
+    for (var i = 2021; i<=3500; i++) {
+        $('.years').append(new Option(i));
+    }
+    for (var i = 1; i<=12; i++) {
+        $('.months').append(new Option(pad(i, 2)));
+    }
+    for (var i =1; i<=31; i++) {
+        $('.days').append(new Option(pad(i, 2)));
+    }
+});
